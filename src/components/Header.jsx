4e40513f750cc3832dc4844386b0bb11ad38b0bc
lonelybuddy <henryzheng92@gmail.com>
2022-02-17 11:32:53 -0600

    created and moved logo to Logo component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
+import { Logo } from './Logo';
 import { RequestButton } from './RequestButton';
 
 export const Header = () => {
   return (
     <header className="max-w-[1190px] mx-auto transition-all flex justify-between items-center px-4 md:px-10 pt-6 md:pt-10">
-      <div className="font-bold text-[32px] leading-[32px] tracking-[-0.44px]">suite</div>
-
+      <Logo />
       <RequestButton buttonStyle="secondary" />
     </header>
   );
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Logo = () => {
+  return <div className="font-bold text-[32px] leading-[32px] tracking-[-0.44px]">suite</div>;
+};
