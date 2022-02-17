cab420e7515a62f1300eb8baa3e95593fa5aa204
lonelybuddy <henryzheng92@gmail.com>
2022-02-16 23:53:58 -0600

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Header } from './components/Header';
 
 export const App = () => {
-  return <div className="bg-purple-fig text-white text-dark font-epilogue">App</div>;
+  return (
+    <div className="bg-white-fig min-h-screen text-white text-dark font-epilogue">
+      <Header />
+    </div>
+  );
 };
