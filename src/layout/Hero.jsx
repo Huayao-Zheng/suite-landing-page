4aef198be4de23e90e2669e831a475e2575f4a78
lonelybuddy <henryzheng92@gmail.com>
2022-02-18 11:49:29 -0600

    added useMediaQuery hook to render mobile and destop images

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+
 import { CompanyStates } from '../components/CompanyStates';
 
+import { useMediaQuery } from '../useMediaQuery';
+
+import HeroImageForMobile from '../images/image-hero-landscape.png';
+import HeroImageForDesktopAndtablet from '../images/image-hero-portrait.png';
+
 export const Hero = () => {
+  const isTabletAndUp = useMediaQuery('(min-width: 768px)');
+
   return (
     <section id="hero" className="myContainer pt-[56px] md:pt-[80px] xl:pt-[67px]">
+      <img
+        src={isTabletAndUp ? HeroImageForDesktopAndtablet : HeroImageForMobile}
+        alt="phone"
+        className="sssssssssssssssssssssss"
+      />
       <CompanyStates />
     </section>
   );
