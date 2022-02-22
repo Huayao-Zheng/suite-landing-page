a0305fea7302c6d41dbad7cb11296f39833612a6
lonelybuddy <henryzheng92@gmail.com>
2022-02-21 23:47:03 -0600

    md:grid

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -6,7 +6,7 @@ import { StatesList } from '../components/StatesList';
 
 export const Hero = () => {
   return (
-    <section id="hero" className="myContainer pt-[56px] md:pt-[80px] xl:pt-[67px]">
+    <section id="hero" className="myContainer pt-[56px] md:pt-[80px] xl:pt-[67px] md:grid">
       <HeroIntro />
       <HeroImage />
       <StatesList />
