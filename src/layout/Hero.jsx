bcdd683eacf644bec85f2064c59acac7b5696ecf
lonelybuddy <henryzheng92@gmail.com>
2022-02-26 23:05:27 -0600

    centered hero section

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -10,7 +10,8 @@ export const Hero = () => {
       id="hero"
       className="myContainer grid- pt-[56px] md:pt-[80px] xl:pt-[67px] 
       grid gap-8
-      md:grid-cols-[408px_77px_204px] md:grid-rows-[1fr_83px] md:gap-0"
+      md:grid-cols-[408px_77px_204px] md:grid-rows-[1fr_83px] md:gap-0 md:place-content-center
+      "
     >
       {/* layout the grid template and pass the grid-itme classes through below's components */}
       <HeroIntro className="md:row-span-1 md:col-start-1 md:col-end-3" />
