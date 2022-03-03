308cf6c4bdb312fd68fbe1037dafa96404677653
lonelybuddy <henryzheng92@gmail.com>
2022-03-03 09:26:18 -0600

    added clamp on Hero title

--- src/components/HeroIntro.jsx
+++ src/components/HeroIntro.jsx
@@ -8,20 +8,18 @@ export const HeroIntro = ({ className = '' }) => {
   return (
     <div className={`flex flex-col items-start gap-[32px] mx-auto md:mx-0 md:pt-[43px] md:gap-[40px] ${className}`}>
       <div
-        // className="text-[38px] leading-[44px] tracking-[-0.53px]
-        // md:text-[56px] md:leading-[64px] md:tracking-[-0.78px]
-        // xl:text-[72px] xl:leading-[78px] xl:tracking-[-1px] z-10"
-
-        className="text-[38px] leading-[44px] tracking-[-0.53px] 
-        md:text-[56px] md:leading-[64px] md:tracking-[-0.78px] 
-        xl:text-[72px] xl:leading-[78px] xl:tracking-[-1px] z-10"
-        // clamp(3.5rem, 5.21vw + 1rem, 4.5rem);
+        className="
+      text-[38px] leading-[44px] tracking-[-0.53px]
+      md:tracking-[-0.78px]
+      md:text-dynamic
+      xl:tracking-[-1px] z-10
+      "
       >
         <p className="inline-block">
           <img
             src={CurvedLine}
             alt="curved lined"
-            className="w-[152px] h-[19px] md:w-[231px] md:h-[29px] xl:w-[287px] xl:h-[36px] ml-auto"
+            className="w-[152px] h-[19px] md:w-curvedLined md:h-curvedLined ml-auto"
           />
           A <strong>super solution</strong>
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,23 @@ module.exports = {
         'purple-123': '#CB30E3',
         'orange-123': '#FFA84E',
       },
+      width: {
+        curvedLined: 'clamp(14.44rem, 7.44vw + 10.87rem, 17.56rem)',
+      },
+      height: {
+        curvedLined: 'clamp(1.81rem, 1.04vw + 1.31rem, 2.25rem)',
+      },
       fontFamily: {
         epilogue: ['Epilogue', 'sans-serif'],
       },
+    },
+    fontSize: {
+      dynamic: [
+        'clamp(3.5rem, 2.38vw + 2.36rem, 4.5rem)',
+        {
+          lineHeight: 'clamp(4rem, 2.08vw + 3rem, 4.88rem)',
+        },
+      ],
     },
   },
   plugins: [],
