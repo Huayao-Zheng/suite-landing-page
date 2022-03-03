acea2be560b7cb1b84e1a8e32d18f93e3c2c003c
lonelybuddy <henryzheng92@gmail.com>
2022-03-03 12:20:58 -0600

    positioned and aligned the Jeremy image with bg pattern

--- src/layout/Testimonial.jsx
+++ src/layout/Testimonial.jsx
@@ -6,22 +6,31 @@ import CurvedLine2 from '../images/pattern-curved-line-2.svg';
 
 export const Testimonial = () => {
   return (
-    <section id="testimonial" className="bg-dark-blue-123 rounded-[15px]">
+    <section id="testimonial" className="bg-dark-blue-123 rounded-[15px] mt-[237px]">
+      <div className="relative">
+        <img
+          className="absolute top-0 left-[50%] translate-x-[-50%] translate-y-[-172px]"
+          src={Jeremy}
+          alt="Jeremy writing on smart phone"
+        />
+        <img className="h-[223px] mx-auto" src={BgBlur} alt="Blur background" />
+      </div>
+
       <div>
-        <img src={Jeremy} alt="Jeremy writing on smart phone" />
-        <img src={BgBlur} alt="Blur background" />
         <img src={CurvedLine2} alt="curved line 2" />
       </div>
 
       <div className="text-cream-white-123">
-        <h2 className="">It just works.</h2>
+        <h2 className="">
+          It just <strong>works.</strong>
+        </h2>
         <blockquote>
           <p>
             “I really like how it is an all-in-one solution that handle many of the tasks that you would normally need
             separate tools to do the same job. This thing is a miracle worker.”
           </p>
           <footer>
-            <cite>Steve Miller</cite>
+            <strong>JEREMY ROBINSON</strong>
             <div>CMO, FYLO</div>
           </footer>
         </blockquote>
